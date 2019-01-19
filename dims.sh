eaab8b7e529a11fd5b042a468d6ee1ee5c4f1685
Ryan Ellis <ellisry@os1.engr.oregonstate.edu>
2019-01-18 18:20:11 -0800

    Initial commit with dims() shell script developed.

---
+++ dims.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+# description: 'dims' function for matrix prog.
+
+function dims(){
+	
+	# check to ensure valid number of args.
+	# citation: used code from below link for ref
+	# https://oregonstate.instructure.com/courses/1706555/pages/1-dot-4-bash-functions
+	# [ $# -ne 1 ] && echo "invalid number of arguments" >&2
+
+	# check to ensure only integer numbers passed in for 2nd arg
+	# citation: used same site as above snippet
+	# [[ $1 =~ [^0-9]+ ]] && echo "argument '$1' is not an integer" >&2
+
+	# get file contents into stdin using this can get either
+	# when file is piped in or if passed as argument
+	dataFilePath="dataFile$$"
+
+	if [ "$#" = "0" ]
+	then
+		cat > "$dataFilePath"
+	elif [ "$#" = "1" ]
+	then
+		dataFilePath=$1
+	fi
+	
+	# declare vars 
+	rowNum=0
+	colNum=0	
+
+	# iterate thru whole matrix
+	# first, reset internal field separator var (IFS)
+	while IFS=$'\n' read -r line;
+	do
+		# increase row counter
+		rowNum=$(expr "$rowNum" + 1)
+
+		# iterate across the first row
+		# don't need to check additional rows bc
+		# we have already verified matrix is valid
+		if [ "$rowNum" = "1"  ]
+		then
+			for data in $line;
+			do
+				
+				# increase column counter
+				colNum=$(expr "$colNum" + 1)
+
+			done
+		fi
+
+	done < "$dataFilePath"
+
+	# output row and column numbers
+	echo "$rowNum $colNum"
+
+	# clean up temp file if piped in
+	if [ "$#" = 0 ]
+	then
+		rm -f "$dataFilePath"
+	fi
+}
+
+"$@"
